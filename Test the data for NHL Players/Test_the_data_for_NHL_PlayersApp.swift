4a903dba6e31e88d1e07a9d9653f63a87fbe87d5
Thomas Chao <Tchao@lcs.on.ca>
2025-04-04 11:11:21 -0400

    Initial Commit

---
+++ Test the data for NHL Players/Test_the_data_for_NHL_PlayersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_the_data_for_NHL_PlayersApp.swift
+//  Test the data for NHL Players
+//
+//  Created by Yuk Yeung Chao on 2025-04-04.
+//
+
+import SwiftUI
+
+@main
+struct Test_the_data_for_NHL_PlayersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
